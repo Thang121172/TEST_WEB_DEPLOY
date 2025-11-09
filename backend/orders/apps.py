56e0f39d2e41169db6d9e5241ee4877cb8f8450c
Hoang Minh Thang <hoangminhthang12a15@gmail.com>
2025-11-10 00:18:34 +0700

    FIX 9

--- backend/orders/apps.py
+++ backend/orders/apps.py
@@ -7,7 +7,7 @@ class OrdersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     
     # SỬA LỖI: Tên (name) phải là đường dẫn module đầy đủ
-    name = "backend.orders"
+    name = "orders"
     verbose_name = "Orders / Checkout / Delivery Flow"
 
     def ready(self):
